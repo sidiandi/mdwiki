4afd0cf3580dd9bc436ec81bb66b73a92ee04837
Jan Baer <jan@janbaer.de>
2013-07-16 21:29:46 +0200

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+{
+  "name": "mdwiki",
+  "description": "A simple markdown driven wiki",
+  "version": "0.0.1",
+  "author": "Jan Baer",
+  "contributors ": [],
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/janbaer/mdwikie"
+  },
+  "licenses": [
+    {
+      "type": "MIT",
+      "url": "http://www.opensource.org/licenses/MIT"
+    }
+  ],
+  "main": "",
+  "engines": {
+    "node": ">0.8"
+  },
+  "dependencies": {
+    "express": "~3.1",
+    "underscore": "~1.4.4",
+    "underscore.string": "~2.3.0",
+    "markdown": "0.4.x"
+  },
+  "devDependencies": {
+    "grunt": "~0.4",
+    "grunt-contrib-jshint": "~0.2",
+    "grunt-contrib-watch": "~0.3.1",
+    "grunt-contrib-qunit": "0.2.1",
+    "grunt-exec": "~0.4.0"
+  }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Overview
+
+This repository contains a **node.js** project for a simple **markdown** based wiki

5e4bee939ccc9ece558c7b50c981dd534231c23e
Jan Baer <jan@janbaer.de>
2013-08-03 18:30:20 +0200

    some obsolete files removed

--- routes/index.js
+++
@@ -1,6 +0,0 @@
-exports.index = function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
-};

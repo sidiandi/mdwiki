cb9fe1fca7ed555b77388df951a93b9c6b395c55
Jan Baer <jan@janbaer.de>
2013-08-08 20:02:10 +0200

    New function getcontentashtml

--- test/pageStorageTests.js
+++ test/pageStorageTests.js
@@ -8,27 +8,61 @@ var fs = require('fs'),
 describe('PageStorageTests', function () {
 
   describe('When an existing page was queried', function () {
+    var sandbox;
 
     beforeEach(function () {
-      sinon.stub(fs, 'existsSync').returns(true);
-      sinon.stub(fs, 'readFile', function (fileName, callback) {
+      sandbox = sinon.sandbox.create();
+      sandbox.stub(fs, 'existsSync').returns(true);
+      sandbox.stub(fs, 'readFile', function (fileName, callback) {
         callback(null, '#Test');
       });
     });
     it('it should return the content', function (done) {
       storage.getPageContent('index')
-        .then(function (html) {
-          should.exists(html);
-          console.log(html);
-          html.should.not.be.empty;
-          html.should.be.eql('<h1>Test</h1>\n');
+        .then(function (markdown) {
+          should.exists(markdown);
+          console.log(markdown);
+          markdown.should.not.be.empty;
+          markdown.should.be.eql('#Test');
         })
         .done(function () {
           done();
         });
     });
 
-    afterEach(function () {
+    afterEach(function (done) {
+      sandbox.restore();
+      done();
     });
   });
+
+  describe('When an existing page was queried', function () {
+      var sandbox;
+
+      beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(fs, 'existsSync').returns(true);
+        sandbox.stub(fs, 'readFile', function (fileName, callback) {
+          callback(null, '#Test');
+        });
+      });
+
+      it('it should return the content', function (done) {
+        storage.getPageContentAsHtml('index')
+          .then(function (html) {
+            should.exists(html);
+            console.log(html);
+            html.should.not.be.empty;
+            html.should.be.eql('<h1>Test</h1>\n');
+          })
+          .done(function () {
+            done();
+          });
+      });
+
+      afterEach(function (done) {
+        sandbox.restore();
+        done();
+      });
+    });
 });

19093b69e1bced6468e9a951a7f52ba00f57af35
Jan Baer <jan@janbaer.de>
2013-08-15 11:57:25 +0200

    Implementation for git.pull

--- test/gitTests.js
+++ test/gitTests.js
@@ -27,76 +27,148 @@ var fs = require('fs'),
   return deferred.promise;
 }*/
 
-describe('tests for git module tests', function () {
+describe('git module tests', function () {
   this.timeout(5000);
 
-  describe('When the clone method was called and the content folder does not exists', function () {
-    var sandbox,
-        gitRepositoryUrl = 'https://github.com/janbaer/mdwiki.wiki.git';
+  describe('clone tests', function () {
+    describe('When the clone method was called and the content folder does not exists', function () {
+      var sandbox,
+          gitRepositoryUrl = 'https://github.com/janbaer/mdwiki.wiki.git';
 
-    beforeEach(function () {
-      sandbox = sinon.sandbox.create();
-    });
-
-    it('should clone the specified repository into the content folder', function (done) {
-      // ARRANGE
-      sandbox.stub(fs, 'exists', function (folder, callback) {
-        callback(false);
+      beforeEach(function () {
+        sandbox = sinon.sandbox.create();
       });
-      var stub = sandbox.stub(child_process, 'exec', function (command, options, callback) {
-        callback(null, 'ok');
-      });
-      var expected = util.format('git clone %s content', gitRepositoryUrl);
 
-      // ACT
-      git.clone(__dirname, 'content', gitRepositoryUrl)
-        .done(function () {
-          // ASSERT
-          stub.calledOnce.should.be.true;
-          stub.calledWithMatch(expected).should.be.true;
-          done();
+      it('should clone the specified repository into the content folder', function (done) {
+        // ARRANGE
+        sandbox.stub(fs, 'exists', function (folder, callback) {
+          callback(false);
         });
+        var stub = sandbox.stub(child_process, 'exec', function (command, options, callback) {
+          callback(null, 'ok');
+        });
+        var expected = util.format('git clone %s content', gitRepositoryUrl);
+
+        // ACT
+        git.clone(__dirname, 'content', gitRepositoryUrl)
+          .done(function () {
+            // ASSERT
+            stub.calledOnce.should.be.true;
+            stub.calledWithMatch(expected).should.be.true;
+            done();
+          });
+      });
+
+      afterEach(function () {
+        sandbox.restore();
+      });
     });
 
-    afterEach(function () {
-      sandbox.restore();
+    describe('When content folder already exists', function () {
+      var sandbox;
+
+      beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+      });
+
+      it('should return an ContentFolderExistsError', function (done) {
+        // ARRANGE
+        sandbox.stub(fs, 'exists', function (folder, callback) {
+          callback(true);
+        });
+        var stub = sandbox.stub(child_process, 'exec', function (command, options, callback) {
+          callback(null, 'ok');
+        });
+
+        var lastError;
+
+        // ACT
+        git.clone(__dirname, 'content', 'git...')
+          .catch(function (error) {
+            lastError = error;
+          })
+          .done(function () {
+            should.exists(lastError);
+            lastError.should.be.an.instanceof(errors.ContentFolderExistsError);
+            stub.callCount.should.be.eql(0);
+            done();
+          });
+
+      });
+
+      afterEach(function () {
+        sandbox.restore();
+      });
     });
   });
 
-  describe('When content folder already exists', function () {
-    var sandbox;
+  describe('pull tests', function () {
+    describe('When pull was called and content was cloned before', function () {
+      var sandbox;
 
-    beforeEach(function () {
-      sandbox = sinon.sandbox.create();
+      beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+      });
+
+      it('should pull the latest changes from the remote repository', function (done) {
+        // ARRANGE
+        sandbox.stub(fs, 'exists', function (folder, callback) {
+          callback(true);
+        });
+        var stub = sandbox.stub(child_process, 'exec', function (command, options, callback) {
+          callback(null, 'ok');
+        });
+
+        // ACT
+        git.pull(__dirname)
+          .done(function () {
+            // ASSERT
+            stub.calledOnce.should.be.true;
+            stub.calledWithMatch('git pull origin master').should.be.true;
+            done();
+          });
+
+      });
+
+      afterEach(function () {
+        sandbox.restore();
+      });
     });
 
-    it('should return an ContentFolderExistsError', function (done) {
-      // ARRANGE
-      sandbox.stub(fs, 'exists', function (folder, callback) {
-        callback(true);
-      });
-      var stub = sandbox.stub(child_process, 'exec', function (command, options, callback) {
-        callback(null, 'ok');
+    describe('When pull was called and content was never cloned before', function () {
+      var sandbox;
+
+      beforeEach(function () {
+        sandbox = sinon.sandbox.create();
       });
 
-      var lastError;
+      it('should return an ContentFolderNotExistsError', function (done) {
+        // ARRANGE
+        sandbox.stub(fs, 'exists', function (folder, callback) {
+          callback(false);
+        });
+        var stub = sandbox.stub(child_process, 'exec', function (command, options, callback) {
+          callback(null, 'ok');
+        });
+        var lastError;
 
-      // ACT
-      git.clone(__dirname, 'content', 'git...')
+        // ACT
+        git.pull(__dirname)
         .catch(function (error) {
           lastError = error;
         })
         .done(function () {
+          // ASSERT
           should.exists(lastError);
-          lastError.should.be.an.instanceof(errors.ContentFolderExistsError);
+          lastError.should.be.an.instanceof(errors.ContentFolderNotExistsError);
           stub.callCount.should.be.eql(0);
           done();
         });
+      });
 
-    });
-
-    afterEach(function () {
-      sandbox.restore();
+      afterEach(function () {
+        sandbox.restore();
+      });
     });
   });
 });

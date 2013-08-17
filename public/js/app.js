bbfd00b0b8ff83b939ba05f5f703c1b0a176bda4
Jan Baer <jan@janbaer.de>
2013-08-17 14:45:39 +0200

    switched to html5 link mode without hash bang

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,28 @@
 'use strict';
 
 var mdwiki = angular.module('mdwiki', [
+  'ngRoute',
   'ngSanitize',
   'mdwiki.controllers',
   'mdwiki.filters',
   'mdwiki.services',
   'mdwiki.directives'
 ]).config(function ($routeProvider, $locationProvider) {
-  $routeProvider.when('/', {
+  $routeProvider
+    .when('/git/clone', {
+      templateUrl: './views/gitclone.html',
+      controller: 'GitCloneCtrl'
+    })
+    .when('/', {
       templateUrl: './views/content.html',
       controller: 'ContentCtrl'
-    }).when('/:page', {
+    })
+    .when('/:page', {
       templateUrl: './views/content.html',
       controller: 'ContentCtrl'
     }).otherwise({
       redirectTo: '/index'
     });
 
-  $locationProvider.html5Mode(false);
+  $locationProvider.html5Mode(true);
 });

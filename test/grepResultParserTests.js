16e2b3ba3079e771075328ddc7f54d603c7ce1e2
Omar Diego Vera Ustariz <omar.vera@gmail.com>
2013-08-20 21:27:54 +0200

    Continued implementation of grepResultParser

--- test/grepResultParserTests.js
+++ test/grepResultParserTests.js
@@ -32,5 +32,107 @@ describe('grep result parser module tests', function () {
             });
 
         });
+
+        describe('When the grep result is not empty the parser should return a valid result object', function () {
+            var grepResult = '';
+
+            beforeEach(function () {
+            });
+
+            it('should return an empty array', function (done) {
+                // ARRANGE
+                grepResult = 'file.md:this was found';
+
+                // ACT
+                grepResultParser.parse(grepResult).done(function(parseResult){
+                    // ASSERT
+                    parseResult.length.should.be.eql(1);
+                    parseResult[0].fileName.should.be.eql('file.md');
+                    parseResult[0].fileContext.should.be.eql('this was found');
+                    done();
+                });
+            });
+
+            afterEach(function () {
+            });
+
+        });
+
+        describe('When the grep result contains the path of the file the parser should return a valid result object', function () {
+            var grepResult = '';
+
+            beforeEach(function () {
+            });
+
+            it('should return an empty array', function (done) {
+                // ARRANGE
+                grepResult = '/path/to/file/file.md:this was found';
+
+                // ACT
+                grepResultParser.parse(grepResult).done(function(parseResult){
+                    // ASSERT
+                    parseResult.length.should.be.eql(1);
+                    parseResult[0].fileName.should.be.eql('file.md');
+                    parseResult[0].fileContext.should.be.eql('this was found');
+                    done();
+                });
+            });
+
+            afterEach(function () {
+            });
+
+        });
+
+        describe('When the grep result contains a windows path of the file the parser should return a valid result object', function () {
+            var grepResult = '';
+
+            beforeEach(function () {
+            });
+
+            it('should return an empty array', function (done) {
+                // ARRANGE
+                grepResult = 'c:/path/to/file/file.md:this was found';
+
+                // ACT
+                grepResultParser.parse(grepResult).done(function(parseResult){
+                    // ASSERT
+                    parseResult.length.should.be.eql(1);
+                    parseResult[0].fileName.should.be.eql('file.md');
+                    parseResult[0].fileContext.should.be.eql('this was found');
+                    done();
+                });
+            });
+
+            afterEach(function () {
+            });
+
+        });
+
+        describe('When the grep result contains multiple lines, then the parser should return a valid list of parsed objects', function () {
+            var grepResult = '';
+
+            beforeEach(function () {
+            });
+
+            it('should return an empty array', function (done) {
+                // ARRANGE
+                grepResult = 'c:/path/to/file/file.md:this was found \n c:/path/to/file/file2.md:this was found as well';
+
+                // ACT
+                grepResultParser.parse(grepResult).done(function(parseResult){
+                    // ASSERT
+                    parseResult.length.should.be.eql(2);
+                    parseResult[0].fileName.should.be.eql('file.md');
+                    parseResult[0].fileContext.should.be.eql('this was found');
+                    parseResult[1].fileName.should.be.eql('file2.md');
+                    parseResult[1].fileContext.should.be.eql('this was found as well');
+                    done();
+                });
+            });
+
+            afterEach(function () {
+            });
+
+        });
     });
 });

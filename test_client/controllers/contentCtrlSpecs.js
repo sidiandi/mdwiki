6c1fb06ff0faff43023c006d7dab0ee64565aa18
Jan Baer <jan@janbaer.de>
2013-08-21 21:20:13 +0200

    Added some tests for the contentCtrl

--- test_client/controllers/contentCtrlSpecs.js
+++ test_client/controllers/contentCtrlSpecs.js
@@ -8,28 +8,100 @@ describe('Content Controller Tests', function () {
   });
 
   describe('When the page exists', function () {
-    var scope, service, $controllerConstructor;
+    var $scope, $controller, pageService;
 
-    beforeEach(inject(function ($injector, $controller, $rootScope, $q) {
-      scope = $rootScope.$new();
-      $controllerConstructor = $controller;
-      service = $injector.get('pageService');
+    beforeEach(inject(function ($injector, $rootScope, $q) {
+      $scope = $rootScope.$new();
+      $controller = $injector.get('$controller');
+
+      pageService = $injector.get('pageService');
       var deferred = $q.defer();
       deferred.resolve('<h1>Test</h1>');
-      spyOn(service, 'getPage').andReturn(deferred.promise);
-      $rootScope.$apply();
+      spyOn(pageService, 'getPage').andReturn(deferred.promise);
     }));
 
     it('should set the html in the content of the scope', function () {
-      var controller = $controllerConstructor('ContentCtrl', {$scope: scope, $routeParams: { page: 'index'}, $location: {}, pageService: service });
+      var controller = $controller('ContentCtrl', {
+        $scope: $scope,
+        $routeParams: { page: 'index'},
+        $location: {},
+        pageService: pageService
+      });
 
-      expect(scope.content).toEqual('<h1>Test</h1>');
-    });
+      // This is important to resolve the promises => it must called after the function
+      // that is using the promise, in this case the constructor function of the controller
+      $scope.$apply();
 
-    afterEach(function () {
+      expect($scope.content).toEqual('<h1>Test</h1>');
     });
   });
 
+  describe('When Page not exists and the page name is index', function () {
+    var $scope, $controller, $location, pageService;
 
+    beforeEach(inject(function ($injector, $rootScope, $q) {
+      $scope = $rootScope.$new();
+      $controller = $injector.get('$controller');
+      $location = $injector.get('$location');
+
+      pageService = $injector.get('pageService');
+      var deferred = $q.defer();
+
+      var error = new Error();
+      error.code = 404;
+      deferred.reject(error);
+      spyOn(pageService, 'getPage').andReturn(deferred.promise);
+
+      spyOn($location, 'path');
+    }));
+
+    it('should redirect the user to the git clone page', function () {
+      var controller = $controller('ContentCtrl', {
+        $scope: $scope,
+        $routeParams: { page: 'index'},
+        $location: $location,
+        pageService: pageService
+      });
+
+      $scope.$apply();
+
+      expect($scope.content).toBeUndefined();
+      expect($location.path).toHaveBeenCalledWith('/git/clone');
+    });
+  });
+
+  describe('When Page not exists and the page name is not index', function () {
+    var $scope, $controller, $location, pageService;
+
+    beforeEach(inject(function ($injector, $rootScope, $q) {
+      $scope = $rootScope.$new();
+      $controller = $injector.get('$controller');
+      $location = $injector.get('$location');
+
+      pageService = $injector.get('pageService');
+      var deferred = $q.defer();
+
+      var error = new Error();
+      error.code = 404;
+      deferred.reject(error);
+      spyOn(pageService, 'getPage').andReturn(deferred.promise);
+
+      spyOn($location, 'path');
+    }));
+
+    it('should redirect the user show that the content was not found', function () {
+      var controller = $controller('ContentCtrl', {
+        $scope: $scope,
+        $routeParams: { page: 'page1'},
+        $location: $location,
+        pageService: pageService
+      });
+
+      $scope.$apply();
+
+      expect($scope.content).toEqual('Content not found!');
+      expect($location.path).not.toHaveBeenCalled();
+    });
+  });
 
 });

ba6c4ce99f82f8caf3d26598f104583392bc41c8
Jan Baer <jan@janbaer.de>
2013-09-09 21:32:03 +0200

    refactor:
    - New directive to show bootstrap tooltips for buttons and links.
    - New directive to switch the text for the pull button while pulling the latest changes
    - jQuery code removed from index.html
    - jQuery code removed from GitPullController

--- public/js/controllers/GitPullCtrl.js
+++ public/js/controllers/GitPullCtrl.js
@@ -22,7 +22,6 @@ controllers.controller('GitPullCtrl', ['$scope', '$route', 'GitService', 'PageSe
       .then(pageService.getPages)
       .then(function () {
         $scope.message = 'The repository was successful update...';
-        $('#pullButton').button('reset');
         $route.reload();
       }, function (error) {
         $scope.message = 'There is an error occured while updating your repository: ' + error.message;
--- public/js/directives.js
+++ public/js/directives.js
@@ -4,8 +4,30 @@
 
 var directives = angular.module('mdwiki.directives', []);
 
-directives.directive('appVersion', function (version) {
-    return function (scope, elm, attrs) {
-      elm.text(version);
-    };
-  });
+directives.directive('bsTooltip', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      element.tooltip({
+        animation: true,
+        placement: 'bottom',
+        delay: { show: 100, hide: 100 }
+      });
+    }
+  };
+});
+
+directives.directive('bsSwitchtext', function () {
+  return {
+    restrict: 'A',
+    link: function postlink(scope, element, attrs) {
+      scope.$watch('isBusy', function (newValue, oldValue) {
+        if (newValue === true) {
+          element.button('loading');
+        } else {
+          element.button('reset');
+        }
+      });
+    }
+  };
+});

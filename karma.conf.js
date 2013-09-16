efb9b678e963156f7710092909313a42b2c85199
Jan Baer <jan@janbaer.de>
2013-09-16 18:14:02 +0200

    refactor: changed karma.conf.js to make it jslint conform

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,15 @@
 // Karma configuration
 // Generated on Fri Aug 16 2013 16:22:37 GMT+0200 (W. Europe Daylight Time)
+'use strict';
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path, that will be used to resolve files and exclude
     basePath: '',
 
-
     // frameworks to use
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -25,34 +24,27 @@ module.exports = function(config) {
       'test_client/**/*Specs.js'
     ],
 
-
     // list of files to exclude
     exclude: [
 
     ],
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
     reporters: ['progress', 'growl'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // Start these browsers, currently available:
     // - Chrome
@@ -64,10 +56,8 @@ module.exports = function(config) {
     // - IE (only Windows)
     browsers: ['ChromeCanary'],
 
-
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
-
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit

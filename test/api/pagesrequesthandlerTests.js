13faea71553e860c9412c03a5a2c40c8fcf44195
Jan Baer <jan@janbaer.de>
2013-09-25 06:18:58 +0200

    refactor(tests): the tests for each requesthandler are now in it own files

---
+++ test/api/pagesrequesthandlerTests.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var request = require('supertest'),
+    express = require('express'),
+    should = require('should'),
+    sinon = require('sinon'),
+    fs = require('fs'),
+    Q = require('q'),
+    storage = require('../../lib/pageStorageFS'),
+    errors = require('../../lib/errors'),
+    pagesRequestHandler = require('../../api/pagesrequesthandler');
+
+describe('pagesrequesthandler tests', function () {
+
+  var app;
+  var sandbox;
+
+  beforeEach(function () {
+    app = express();
+    app.use(express.bodyParser());
+
+    app.get('/api/pages', pagesRequestHandler);
+
+    sandbox = sinon.sandbox.create();
+  });
+
+  describe('When user wants to list all existing pages', function () {
+    beforeEach(function () {
+      sandbox.stub(storage, 'getPages', function () {
+        var d = Q.defer();
+        d.resolve([ {name: 'page1'}, {name: 'page2'}, {name: 'index'}, {name: 'home'}, {name: 'readme'}]);
+        return d.promise;
+      });
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+    });
+
+    it('should return a list of the pages', function (done) {
+      request(app).get('/api/pages')
+        .expect('Content-Type', "application/json")
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          var pages = res.body;
+
+          should.exists(pages);
+
+          pages.length.should.equal(5);
+
+          done();
+        });
+    });
+  });
+
+});
+
+
+

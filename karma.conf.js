2d804eb29a71e0fe36db79a2297e22c5b2bc1263
Jan Baer <jan@janbaer.de>
2013-09-28 14:43:11 +0200

    chore(karma): added the karma-spec-reporter to package.json and switched to this reporter to get a more detailled output about the executed tests

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress', 'growl'],
+    reporters: ['spec', 'growl'],
 
     // web server port
     port: 9876,

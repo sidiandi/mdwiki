37d59f4332a7bb3fe829c9ef64ad69c0563635f0
Omar Diego Vera Ustariz <omar.vera@gmail.com>
2013-11-02 20:52:13 +0100

    Removed unused console logs

--- test/lib/grepSearchTests.js
+++ test/lib/grepSearchTests.js
@@ -42,7 +42,6 @@ describe('grep search module tests', function () {
               // ASSERT
               stub.calledOnce.should.be.true;
               stub.calledWithMatch(expected).should.be.true;
-              console.log('Data......:' + data);
               data.should.be.eql('Text with Java');
               done();
             });

52484a0bef10246b7cda93b5fd6890f6d7548ddf
Jan Baer <jan@janbaer.de>
2013-11-08 18:17:11 +0100

    feat(gitprovider): Implemented the contentproviderfactory with tests

---
+++ test/lib/contentProviderFactoryTests.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var factory = require('../../lib/contentProviderFactory'),
+    errors = require('../../lib/errors'),
+    should = require('should');
+
+describe('provider factory tests', function () {
+
+  describe('When the git provider was given ', function () {
+    it('should return the git provider', function () {
+      var provider = factory.create('Git');
+      should.exists(provider);
+    });
+  });
+
+  describe('When the github provider was given ', function () {
+    it('should return the github provider', function () {
+      var provider = factory.create('GitHub');
+      should.exists(provider);
+    });
+  });
+
+  describe('When a unknown provider was given ', function () {
+    it('should throw an UnknownProviderError', function () {
+      /*jshint -W068 */
+      (function () {
+        factory.create('providerXYZ');
+      }).should.throw(errors.UnkownProviderError);
+    });
+  });
+});
+

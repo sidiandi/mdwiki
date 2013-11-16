e4569a6b26826096159b03e1d52b9c5ac4f47801
Jan Baer <jan@janbaer.de>
2013-11-16 13:51:40 +0100

    feat(staticfiles): githubUrlBuilder supports now a method to build url for static files, tests written

---
+++ test/lib/githubUrlBuilderTests.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var urlBuilder = require('../../lib/githubUrlBuilder');
+
+describe('github UrlBuilder Tests', function () {
+  var GITHUB_USER = 'janbaer', GITHUB_REPOSITORY = 'wiki-content';
+
+  describe('When a url for a static file is given', function () {
+    it('Should return the raw url for the github request', function () {
+      var expectedUrl = 'https://github.com/janbaer/wiki-content/raw/master/static/pdf/PowerShell_ISE_v3.pdf';
+      var givenUrl = '/static/janbaer/wiki-content/pdf/PowerShell_ISE_v3.pdf';
+
+      var actual = urlBuilder.buildStaticFileUrl(GITHUB_USER, GITHUB_REPOSITORY, givenUrl);
+
+      actual.should.be.equal(expectedUrl);
+    });
+  });
+
+  describe('When a url for the content of the repository is required', function () {
+    it('Should return the expected url', function () {
+      var expectedUrl = 'https://api.github.com/repos/janbaer/wiki-content/contents';
+      var actual = urlBuilder.buildPagesUrl(GITHUB_USER, GITHUB_REPOSITORY);
+
+      actual.should.be.equal(expectedUrl);
+    });
+  });
+
+  describe('When the url for the content of a page is required', function () {
+    it('Should return the expected url', function () {
+      var expectedUrl = 'https://raw.github.com/janbaer/wiki-content/master/index.md';
+      var actual = urlBuilder.buildPageContentUrl(GITHUB_USER, GITHUB_REPOSITORY, 'index');
+
+      actual.should.be.equal(expectedUrl);
+    });
+  });
+
+  describe('When the url to search in the repository is required', function () {
+    it('Should return the expected url', function () {
+      var expectedUrl = 'https://api.github.com/search/code?q=git+in:file+extension:md+repo:janbaer/wiki-content';
+      var actual = urlBuilder.buildSearchUrl(GITHUB_USER, GITHUB_REPOSITORY, 'git');
+
+      actual.should.be.equal(expectedUrl);
+    });
+  });
+});

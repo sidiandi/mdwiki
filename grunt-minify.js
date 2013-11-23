caeac19bdb6bbb2c2251b7551a2f0dc44f5dab49
Jan Baer <jan@janbaer.de>
2013-11-23 13:24:48 +0100

    chore: Added a postinstall script to npm for file minification after deployment

---
+++ grunt-minify.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'public/css/',
+        src: ['styles.css'],
+        dest: 'public/css/',
+        ext: '.min.css'
+      }
+    },
+
+    uglify: {
+      scripts: {
+        options: {
+          // the banner is inserted at the top of the output
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        },
+        files: {
+          'public/js/scripts.min.js': ['public/js/scripts.js'],
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['cssmin', 'uglify']);
+
+};

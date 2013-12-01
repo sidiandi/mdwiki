976aecbc699c21a4c79b320c2529b16a9371a841
Jan Baer <jan@janbaer.de>
2013-12-01 19:15:52 +0100

    feat(serverconfig): Implemented a angular service to read the server config

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -32373,6 +32373,37 @@ services.factory('SearchService', ['$http', '$q', 'ApiUrlBuilderService', functi
 
 var services = services || angular.module('mdwiki.services', []);
 
+services.factory('ServerConfigService', ['$http', '$q', function ($http, $q) {
+  var getConfig = function (page) {
+    var deferred = $q.defer();
+
+    $http({
+      method: 'GET',
+      url: '/api/serverconfig',
+      headers: {'Content-Type': 'application/json'},
+    })
+    .success(function (data, status, headers, config) {
+      deferred.resolve(JSON.parse(data.toString()));
+    })
+    .error(function (data, status, headers, config) {
+      var error = new Error();
+      error.message = status === 404 ? 'Content not found' : 'Unexpected server error';
+      error.code = status;
+      deferred.reject(error);
+    });
+
+    return deferred.promise;
+  };
+
+  return {
+    getConfig: getConfig
+  };
+}]);
+
+'use strict';
+
+var services = services || angular.module('mdwiki.services', []);
+
 services.factory('SettingsService', ['$angularCacheFactory', function ($angularCacheFactory) {
   var cache = $angularCacheFactory('mdwiki', { storageMode: 'localStorage' });
 
---
+++ public/js/services/serverconfigservice.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var services = services || angular.module('mdwiki.services', []);
+
+services.factory('ServerConfigService', ['$http', '$q', function ($http, $q) {
+  var getConfig = function (page) {
+    var deferred = $q.defer();
+
+    $http({
+      method: 'GET',
+      url: '/api/serverconfig',
+      headers: {'Content-Type': 'application/json'},
+    })
+    .success(function (data, status, headers, config) {
+      deferred.resolve(data);
+    })
+    .error(function (data, status, headers, config) {
+      var error = new Error();
+      error.message = status === 404 ? 'Content not found' : 'Unexpected server error';
+      error.code = status;
+      deferred.reject(error);
+    });
+
+    return deferred.promise;
+  };
+
+  return {
+    getConfig: getConfig
+  };
+}]);

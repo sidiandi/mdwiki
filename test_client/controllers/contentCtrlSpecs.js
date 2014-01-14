2cef83fd2cea5f0f9fb86fb8df74ddef2bc26940
Jan Baer <jan@janbaer.de>
2014-01-14 20:50:04 +0100

    refact(tests): ContentCtrlSpecs rewritten

--- test_client/controllers/contentCtrlSpecs.js
+++ test_client/controllers/contentCtrlSpecs.js
@@ -8,69 +8,71 @@ describe('Content Controller Tests', function () {
   });
 
   describe('When the page exists', function () {
-    var $scope, $controller, deferred, pageService, settingsService;
+    var createController, $scope;
 
     beforeEach(inject(function ($injector, $rootScope, $q) {
       $scope = $rootScope.$new();
-      $controller = $injector.get('$controller');
+      var $controller = $injector.get('$controller');
 
-      pageService = $injector.get('PageService');
-      deferred = $q.defer();
-      spyOn(pageService, 'getPage').andReturn(deferred.promise);
+      var pageService = $injector.get('PageService');
+      spyOn(pageService, 'getPage').andReturn($q.when('<h1>Test</h1>'));
 
-      settingsService = $injector.get('SettingsService');
+      var settingsService = $injector.get('SettingsService');
       spyOn(settingsService, 'get').andReturn({ provider: 'git' });
+
+      createController = function () {
+        return $controller('ContentCtrl', {
+          $rootScope: $rootScope,
+          $scope: $scope,
+          $routeParams: { page: 'index'},
+          $location: {},
+          pageService: pageService,
+          settingsService: settingsService
+        });
+      };
     }));
 
     it('should set the html in the content of the scope', function () {
-      var controller = $controller('ContentCtrl', {
-        $scope: $scope,
-        $routeParams: { page: 'index'},
-        $location: {},
-        pageService: pageService,
-        settingsService: settingsService
-      });
+      var controller = createController();
 
       // This is important to resolve the promises => it must called after the function
       // that is using the promise, in this case the constructor function of the controller
-      $scope.$apply(function () {
-        deferred.resolve('<h1>Test</h1>');
-      });
+      $scope.$apply();
 
       expect($scope.content).toEqual('<h1>Test</h1>');
     });
   });
 
   describe('When Page not exists and the page name is index', function () {
-    var $scope, $controller, $location, pageService, settingsService;
+    var $scope, $location, createController;
 
     beforeEach(inject(function ($injector, $rootScope, $q) {
       $scope = $rootScope.$new();
-      $controller = $injector.get('$controller');
       $location = $injector.get('$location');
+      var $controller = $injector.get('$controller');
 
-      pageService = $injector.get('PageService');
-      var deferred = $q.defer();
+      var pageService = $injector.get('PageService');
 
-      var error = new Error();
-      error.code = 404;
-      deferred.reject(error);
-      spyOn(pageService, 'getPage').andReturn(deferred.promise);
+      spyOn(pageService, 'getPage').andReturn($q.reject({ code: 404}));
 
-      settingsService = $injector.get('SettingsService');
-      spyOn(settingsService, 'get').andReturn({ provider: 'git' });
+      var settingsService = $injector.get('SettingsService');
 
       spyOn($location, 'path');
+
+      createController = function () {
+        return $controller('ContentCtrl', {
+          $rootScope: $rootScope,
+          $scope: $scope,
+          $routeParams: { page: 'index'},
+          $location: $location,
+          pageService: pageService,
+          settingsService: settingsService
+        });
+      };
     }));
 
     it('should redirect the user to the git clone page', function () {
-      var controller = $controller('ContentCtrl', {
-        $scope: $scope,
-        $routeParams: { page: 'index'},
-        $location: $location,
-        pageService: pageService,
-        settingsService: settingsService
-      });
+      var controller = createController();
 
       $scope.$apply();
 
@@ -80,35 +82,35 @@ describe('Content Controller Tests', function () {
   });
 
   describe('When Page not exists and the page name is not index', function () {
-    var $scope, $controller, $location, pageService, settingsService;
+    var $scope, $location, createController;
 
     beforeEach(inject(function ($injector, $rootScope, $q) {
       $scope = $rootScope.$new();
-      $controller = $injector.get('$controller');
       $location = $injector.get('$location');
 
-      pageService = $injector.get('PageService');
-      var deferred = $q.defer();
+      var $controller = $injector.get('$controller');
+      var pageService = $injector.get('PageService');
 
-      var error = new Error();
-      error.code = 404;
-      deferred.reject(error);
-      spyOn(pageService, 'getPage').andReturn(deferred.promise);
+      spyOn(pageService, 'getPage').andReturn($q.reject({ code: 404 }));
 
-      settingsService = $injector.get('SettingsService');
-      spyOn(settingsService, 'get').andReturn({ provider: 'git' });
+      var settingsService = $injector.get('SettingsService');
 
       spyOn($location, 'path');
+
+      createController = function () {
+        return $controller('ContentCtrl', {
+          $rootScope: $rootScope,
+          $scope: $scope,
+          $routeParams: { page: 'page1'},
+          $location: $location,
+          pageService: pageService,
+          settingsService: settingsService
+        });
+      };
     }));
 
-    it('should redirect the user show that the content was not found', function () {
-      var controller = $controller('ContentCtrl', {
-        $scope: $scope,
-        $routeParams: { page: 'page1'},
-        $location: $location,
-        pageService: pageService,
-        settingsService: settingsService
-      });
+    it('should the user show that the content was not found', function () {
+      var controller = createController();
 
       $scope.$apply();
 
@@ -118,29 +120,28 @@ describe('Content Controller Tests', function () {
   });
 
   describe('When the user navigates to a page', function () {
-    var createController, rootScope;
+    var createController, $rootScope;
 
     beforeEach(inject(function ($injector, $q) {
-      rootScope = $injector.get('$rootScope');
+      $rootScope = $injector.get('$rootScope');
       var $controller = $injector.get('$controller');
       var $location = $injector.get('$location');
 
-      var $scope = rootScope.$new();
+      var $scope = $rootScope.$new();
 
       var pageService = $injector.get('PageService');
 
       spyOn(pageService, 'getPage').andReturn($q.when('<h1>Test</h1>'));
 
-      spyOn(rootScope, '$broadcast').andCallThrough();
+      spyOn($rootScope, '$broadcast').andCallThrough();
 
       var settingsService = $injector.get('SettingsService');
-      spyOn(settingsService, 'get').andReturn({ provider: 'git' });
 
       createController = function () {
         return $controller('ContentCtrl', {
-          $rootScope: rootScope,
+          $rootScope: $rootScope,
           $scope: $scope,
-          $routeParams: { page: 'page1'},
+          $routeParams: { page: 'index'},
           $location: $location,
           pageService: pageService,
           settingsService: settingsService
@@ -151,49 +152,49 @@ describe('Content Controller Tests', function () {
     it('Should send a message on the global scope, that the editor is hidden', function () {
       var isEditorVisible = true;
 
-      rootScope.$on('isEditorVisible', function (event, data) {
+      $rootScope.$on('isEditorVisible', function (event, data) {
         isEditorVisible = data.isEditorVisible;
       });
 
       var controller = createController();
 
-      rootScope.$digest();
+      $rootScope.$apply();
 
-      expect(rootScope.$broadcast).toHaveBeenCalledWith('isEditorVisible', { isEditorVisible: false });
+      expect($rootScope.$broadcast).toHaveBeenCalledWith('isEditorVisible', { isEditorVisible: false });
       expect(isEditorVisible).toEqual(false);
     });
   });
 
   describe('When html contains some anchors to static files', function () {
-    var $scope, $controller, deferred, pageService, settingsService;
+    var $scope, settingsService, pageService, createController;
 
     beforeEach(inject(function ($injector, $rootScope, $q) {
-      $scope = $rootScope;
-      $controller = $injector.get('$controller');
+      $scope = $rootScope.$new();
+      var $controller = $injector.get('$controller');
 
       pageService = $injector.get('PageService');
-      deferred = $q.defer();
-      spyOn(pageService, 'getPage').andReturn(deferred.promise);
+      spyOn(pageService, 'getPage').andReturn($.when('<a href="/static/staticfile.pdf">Test</a>'));
 
       settingsService = $injector.get('SettingsService');
+
+      createController = function () {
+        return $controller('ContentCtrl', {
+          $rootScope: $rootScope,
+          $scope: $scope,
+          $routeParams: { page: 'index'},
+          $location: {},
+          pageService: pageService,
+          settingsService: settingsService
+        });
+      };
     }));
 
     it('should add a target attribute to the anchors', function () {
       spyOn(settingsService, 'get').andReturn({ provider: 'git' });
 
-      var controller = $controller('ContentCtrl', {
-        $scope: $scope,
-        $routeParams: { page: 'index'},
-        $location: {},
-        pageService: pageService,
-        settingsService: settingsService
-      });
+      var controller = createController();
 
-      // This is important to resolve the promises => it must called after the function
-      // that is using the promise, in this case the constructor function of the controller
-      $scope.$apply(function () {
-        deferred.resolve('<a href="/static/staticfile.pdf">Test</a>');
-      });
+      $scope.$apply();
 
       expect($scope.content).toEqual('<a href="/static/staticfile.pdf" target="_blank">Test</a>');
     });
@@ -202,17 +203,9 @@ describe('Content Controller Tests', function () {
       it('Should add the github username and repository to the url', function () {
         spyOn(settingsService, 'get').andReturn({ provider: 'github', githubUser: 'janbaer', githubRepository: 'wiki' });
 
-        var controller = $controller('ContentCtrl', {
-          $scope: $scope,
-          $routeParams: { page: 'index'},
-          $location: {},
-          pageService: pageService,
-          settingsService: settingsService,
-        });
+        var controller = createController();
 
-        $scope.$apply(function () {
-          deferred.resolve('<a href="/static/staticfile.pdf">Test</a>');
-        });
+        $scope.$apply();
 
         expect($scope.content).toEqual('<a href="/static/janbaer/wiki/staticfile.pdf" target="_blank">Test</a>');
       });
@@ -225,32 +218,33 @@ describe('Content Controller Tests', function () {
   });
 
   describe('When we host a github wiki it should remove the wiki at the begin of the link', function () {
-    var $scope, $controller, deferred, pageService, settingsService;
+    var $scope, createController;
 
     beforeEach(inject(function ($injector, $rootScope, $q) {
       $scope = $rootScope.$new();
-      $controller = $injector.get('$controller');
+      var $controller = $injector.get('$controller');
 
-      pageService = $injector.get('PageService');
-      deferred = $q.defer();
-      spyOn(pageService, 'getPage').andReturn(deferred.promise);
+      var pageService = $injector.get('PageService');
+      spyOn(pageService, 'getPage').andReturn($q.when('<a href="wiki/page1">Page1</a>'));
 
-      settingsService = $injector.get('SettingsService');
-      spyOn(settingsService, 'get').andReturn({ provider: 'git' });
+      var settingsService = $injector.get('SettingsService');
+
+      createController = function () {
+        return $controller('ContentCtrl', {
+          $rootScope: $rootScope,
+          $scope: $scope,
+          $routeParams: { page: 'index'},
+          $location: {},
+          pageService: pageService,
+          settingsService: settingsService
+        });
+      };
     }));
 
     it('should add a target attribut to the anchors', function () {
-      var controller = $controller('ContentCtrl', {
-        $scope: $scope,
-        $routeParams: { page: 'index'},
-        $location: {},
-        pageService: pageService,
-        settingsService: settingsService
-      });
+      var controller = createController();
 
-      $scope.$apply(function () {
-        deferred.resolve('<a href="wiki/page1">Page1</a>');
-      });
+      $scope.$apply();
 
       expect($scope.content).toEqual('<a href="/page1">Page1</a>');
     });

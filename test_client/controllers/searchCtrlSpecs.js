ad1cc74c67df95df44b5e075ab859b51aaeb7c10
Jan Baer <jan@janbaer.de>
2014-02-02 12:47:16 +0100

    fix(search): Search now works also on the search page
    - Fixed the problem that nothing happens when the users is on the
      searchpage
      and enters another searchTerm
    
    Closes #60

--- test_client/controllers/searchCtrlSpecs.js
+++ test_client/controllers/searchCtrlSpecs.js
@@ -8,43 +8,68 @@ describe('Search Controller Tests', function () {
       });
 
     describe('When the user enters a text to be searched', function () {
-        var $scope, $location, $route, searchCtrl, searchService, searchServiceDeferred;
+        var $scope, $location, $route,
+            createController, searchService;
 
         beforeEach(inject(function ($injector, $controller, $rootScope, $q) {
-            $scope = $rootScope.$new();
+          $scope = $rootScope.$new();
 
-            $location = $injector.get('$location');
+          $location = $injector.get('$location');
+          $route = $injector.get('$route');
 
+          searchService = $injector.get('SearchService');
+          spyOn(searchService, 'search').andReturn($q.when([{fileNameWithoutExtension: 'name', fileContext: 'context'}]));
+
+          var $http = $injector.get('$httpBackend');
+          $http.expectGET('./views/searchResult.html').respond(200, '</h1>');
+
+          createController = function () {
+            return $controller('SearchCtrl', {
+              $scope: $scope,
+              $location: $location,
+              $route: $route,
+              searchService: searchService
+            });
+          };
+        }));
+
+        describe('When the user is not on the search page', function () {
+          it('Should call the search service and navigates to the search page', function () {
+            // ARRANGE
             spyOn($location, 'path');
+            createController();
+            var textToSearch = 'textToSearch';
+            $scope.textToSearch = textToSearch;
 
-            $route = $injector.get('$route');
-
-            // spyOn($route, 'reload');
-
-            searchService = $injector.get('SearchService');
-            searchServiceDeferred = $q.defer();
-            spyOn(searchService, 'search').andReturn(searchServiceDeferred.promise);
-
-            var $http = $injector.get('$httpBackend');
-            $http.expectGET('./views/searchResult.html').respond(200, '</h1>');
-
-            searchCtrl = $controller('SearchCtrl', {
-                $scope: $scope,
-                $location: $location,
-                $route: $route,
-                searchService: searchService
-              });
-          }));
-
-        it('should call the search method and search location successfully', function () {
-            $scope.textToSearch = 'textToSearch';
+            // ACT
             $scope.search();
-
-            searchServiceDeferred.resolve([{fileNameWithoutExtension: 'name', fileContext: 'context'}]);
             $scope.$digest();
 
+            // ASSERT
             expect($location.path).toHaveBeenCalledWith('/search');
-            expect(searchService.search).toHaveBeenCalled();
+            expect(searchService.search).toHaveBeenCalledWith(textToSearch);
           });
+        });
+
+        describe('When the user is on the search page', function () {
+          it('Should call the search service and reload the search page', function () {
+            // ARRANGE
+            spyOn($location, 'path').andReturn('/search');
+            spyOn($route, 'reload');
+
+            createController();
+            var textToSearch = 'textToSearch';
+            $scope.textToSearch = textToSearch;
+
+            // ACT
+            $scope.search();
+            $scope.$digest();
+
+            // ASSERT
+            expect($location.path).not.toHaveBeenCalledWith('/search');
+            expect($route.reload).toHaveBeenCalled();
+            expect(searchService.search).toHaveBeenCalledWith(textToSearch);
+          });
+        });
       });
   });

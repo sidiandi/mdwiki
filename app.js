a25abe0e4b71f9bdfa1c7e7d5789c2d386be3b54
Jan Baer <jan@janbaer.de>
2014-04-26 18:47:15 +0200

    Extracted the definition of routes to expresssetup.js

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 'use strict';
 
 var express = require('express'),
-    path = require('path'),
     logger = require('./lib/logger'),
-    util = require('util'),
     oauth = require('./lib/oauth'),
-    expressSetup = require('./expresssetup.js'),
-    pageRequestHandler = require('./api/pagerequesthandler'),
-    pagesRequestHandler = require('./api/pagesrequesthandler'),
-    gitRequestHandler = require('./api/gitrequesthandler'),
-    searchRequestHandler = require('./api/searchrequesthandler'),
-    staticFileRequestHandler = require('./api/staticfilerequesthandler'),
-    serverConfigRequestHandler = require('./api/serverconfigrequesthandler');
+    expressSetup = require('./expresssetup.js');
 
 var app = express();
 
@@ -21,68 +13,14 @@ oauth.setup(['github'], isProductionMode ? require('./config/oauthconfig.json') 
 
 expressSetup.middleware(app, isProductionMode);
 expressSetup.staticRoutes(app);
-
-
-app.get('/js/scripts.js', function (req, res) {
-  if (isProductionMode) {
-    logger.info('Send minified script');
-    res.sendfile('./public/js/scripts.min.js');
-  } else {
-    res.sendfile('./public/js/scripts.js');
-  }
-});
-
-app.get('/css/styles.css', function (req, res) {
-  if (isProductionMode) {
-    logger.info('Send minified styles');
-    res.sendfile('./public/css/styles.min.css');
-  }
-  else {
-    res.sendfile('./public/css/styles.css');
-  }
-});
-
-// Authentication routes
-app.get('/auth/user', oauth.user);
-app.delete('/auth/user', oauth.logout);
-
-// JSON API
-app.get('/api/serverconfig', serverConfigRequestHandler);
-app.get('/api/pages', pagesRequestHandler);
-app.get('/api/page/:page?', pageRequestHandler.get);
-app.put('/api/page/:page', oauth.ensureAuthentication, pageRequestHandler.put);
-app.delete('/api/page/:page', oauth.ensureAuthentication, pageRequestHandler.delete);
-
-app.get('/api/:githubUser/:githubRepository/pages', pagesRequestHandler);
-app.get('/api/:githubUser/:githubRepository/page/:page?', pageRequestHandler.get);
-app.put('/api/:githubUser/:githubRepository/page/:page', oauth.ensureAuthentication, pageRequestHandler.put);
-app.delete('/api/:githubUser/:githubRepository/page/:page', oauth.ensureAuthentication, pageRequestHandler.delete);
-
-app.post('/api/search', searchRequestHandler.search);
-app.post('/api/:githubUser/:githubRepository/search', searchRequestHandler.search);
-
-app.post('/api/git/clone', gitRequestHandler.clone);
-app.post('/api/git/pull', gitRequestHandler.pull);
-
-// Static pages
-app.get('/static/:githubUser/:githubRepository/*', staticFileRequestHandler);
-app.get('/static/*', staticFileRequestHandler);
-
-// Routes that should just send the index.html
-app.get('/git/clone', function (req, res) {
-  res.sendfile('./public/index.html');
-});
-
-app.get('*', function (req, res) {
-  res.sendfile('./public/index.html');
-});
+expressSetup.defineRoutes(app, oauth, isProductionMode);
 
 var port = process.env.PORT || 3000;
+var hostname = process.env.HOST;
 
 logger.info('Starting server in %s mode', app.get('env'));
 
-if (process.env.HOST !== undefined) {
-  var hostname = process.env.HOST;
+if (hostname) {
   app.listen(port, hostname, function () {
     logger.info('Listening on port %s over hostname %s', port, hostname);
   });
--- expresssetup.js
+++ expresssetup.js
@@ -2,7 +2,14 @@
 
 var express = require('express'),
     everyauth = require('everyauth'),
-    path = require('path');
+    logger = require('./lib/logger'),
+    path = require('path'),
+    pageRequestHandler = require('./api/pagerequesthandler'),
+    pagesRequestHandler = require('./api/pagesrequesthandler'),
+    gitRequestHandler = require('./api/gitrequesthandler'),
+    searchRequestHandler = require('./api/searchrequesthandler'),
+    staticFileRequestHandler = require('./api/staticfilerequesthandler'),
+    serverConfigRequestHandler = require('./api/serverconfigrequesthandler');
 
 module.exports.middleware = function (app, isProductionMode) {
   app.use(require('body-parser')());
@@ -27,4 +34,67 @@ module.exports.staticRoutes = function (app) {
   app.use('/font', express.static(path.join(__dirname, 'public/font')));
   app.use('/views', express.static(path.join(__dirname, 'public/views')));
   app.use('/images', express.static(path.join(__dirname, 'public/images')));
+};
+
+module.exports.defineRoutes = function (app, oauth, isProductionMode) {
+  app.get('/js/scripts.js', function (req, res) {
+    if (isProductionMode) {
+      logger.info('Send minified script');
+      res.sendfile('./public/js/scripts.min.js');
+    } else {
+      res.sendfile('./public/js/scripts.js');
+    }
+  });
+
+  app.get('/css/styles.css', function (req, res) {
+    if (isProductionMode) {
+      logger.info('Send minified styles');
+      res.sendfile('./public/css/styles.min.css');
+    }
+    else {
+      res.sendfile('./public/css/styles.css');
+    }
+  });
+
+  // Authentication routes
+  app.get('/auth/user', oauth.user);
+  app.delete('/auth/user', oauth.logout);
+
+  // JSON API
+  app.get('/api/serverconfig', serverConfigRequestHandler);
+
+  app.get('/api/pages', pagesRequestHandler);
+  app.get('/api/page/:page?', pageRequestHandler.get);
+
+  app.route('/api/page/:page')
+    .all(oauth.ensureAuthentication)
+    .put(pageRequestHandler.put)
+    .delete(pageRequestHandler.delete);
+
+  app.get('/api/:githubUser/:githubRepository/pages', pagesRequestHandler);
+  app.get('/api/:githubUser/:githubRepository/page/:page?', pageRequestHandler.get);
+
+  app.route('/api/:githubUser/:githubRepository/page/:page')
+    .all(oauth.ensureAuthentication)
+    .put(pageRequestHandler.put)
+    .delete(pageRequestHandler.delete);
+
+  app.post('/api/search', searchRequestHandler.search);
+  app.post('/api/:githubUser/:githubRepository/search', searchRequestHandler.search);
+
+  app.post('/api/git/clone', gitRequestHandler.clone);
+  app.post('/api/git/pull', gitRequestHandler.pull);
+
+  // Static pages
+  app.get('/static/:githubUser/:githubRepository/*', staticFileRequestHandler);
+  app.get('/static/*', staticFileRequestHandler);
+
+  // Routes that should just send the index.html
+  app.get('/git/clone', function (req, res) {
+    res.sendfile('./public/index.html');
+  });
+
+  app.get('*', function (req, res) {
+    res.sendfile('./public/index.html');
+  });
 };
